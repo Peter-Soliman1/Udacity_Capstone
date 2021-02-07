a53b8ea953bf82f8ad257e7afd8f7c4fa9b75e77
Ahmed Saeed <engasa3ed@gmail.com>
2021-02-08 01:05:16 +0200

    u

---
+++ create_EKS.sh
@@ -0,0 +1,13 @@
+eksctl create cluster \
+--name udacitycluster \
+--version 1.18 \
+--nodegroup-name standard-workers \
+--node-type t2.small \
+--nodes 2 \
+--nodes-min 1 \
+--nodes-max 3 \
+--node-ami auto \
+--region us-west-2 \
+--zones us-west-2a \
+--zones us-west-2b \
+--zones us-west-2c \
